be61b7c1f1712af625d564cd5f603da406b0de8a
kangyujeong7 <jeong4696@gmail.com>
2024-09-30 14:33:53 +0900

    Title: 배열 채우고 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/배열 채우고 출력하기/filling-array-and-print.py
+++ 240930/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,11 +1,6 @@
-# 예제 입력값
-arr = ["apple", "banana", "cherry", "date"]
+arr = input().strip().split()
 
-# for문과 reversed() 함수를 사용해 배열을 역순으로 순회
-reversed_arr = []
-for element in reversed(arr):
-    reversed_arr.append(element)
+for i in range(9,-1,-1):
+    print(arr[i],end="")
 
-# 결과 출력
-print(reversed_arr)
-ㅔ갸ㅜ
+arr = input()
