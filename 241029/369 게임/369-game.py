3be07c6a7847920329fe5e797da4f16ba7cb3ade
kangyujeong7 <jeong4696@gmail.com>
2024-10-29 22:21:02 +0900

    Title: 369 게임, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 241029/369 게임/369-game.py
+++ 241029/369 게임/369-game.py
@@ -1,21 +1,11 @@
 n = int(input().strip())
 
-
-
-
-
-for i in range(1,n+1):
-    if i%3 == 0: 
-        print(0,end=" ")
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print(0, end=" ")
     else:
-        i_str = str(i)                
-        k =  i_str.split()
-        t = len(k)
-
-        if t == 2 :
-            if k[0] == '3'or'6' or '9':
-                 print(0,end=" ")
-            elif k[1] == '3'or'6' or '9':
-                 print(0,end=" ")
+        i_str = str(i)
+        if any(digit in '369' for digit in i_str):
+            print(0, end=" ")
         else:
-            print(i,end=" ")
+            print(i, end=" ")
