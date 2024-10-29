c09ce709ae5b417e24d59bf9dd9ee0d51a6216c3
kangyujeong7 <jeong4696@gmail.com>
2024-10-29 22:33:03 +0900

    Title: 369 게임, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241029/369 게임/369-game.py
+++ 241029/369 게임/369-game.py
@@ -17,5 +17,7 @@ for i in range(1,n+1):
                  print(0,end=" ")
             elif k[1] == '3'or k[1] =='6' or k[1] =='9':
                  print(0,end=" ")
+            else:
+                print(i, end=" ")
         else:
             print(i,end=" ")
