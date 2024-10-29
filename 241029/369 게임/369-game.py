9a431c7c5970868953f8e005215759a0faf06fea
kangyujeong7 <jeong4696@gmail.com>
2024-10-29 22:48:50 +0900

    Title: 369 게임, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 241029/369 게임/369-game.py
+++ 241029/369 게임/369-game.py
@@ -1,23 +1,21 @@
 n = int(input().strip())
 
-
-
-
-
-for i in range(1,n+1):
-    if i%3 == 0: 
-        print(0,end=" ")
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print(0, end=" ")
     else:
-        i_str = str(i)                
-        k =  i_str.split()
-        t = len(k)
+        i_str = str(i)  # 숫자를 문자열로 변환
+        t = len(i_str)  # 문자열의 길이를 통해 자릿수 확인
 
-        if t == 2 :
-            if k[0] == '3'or k[0] =='6' or k[0]=='9':
-                 print(0,end=" ")
-            elif k[1] == '3'or k[1] =='6' or k[1] =='9':
-                 print(0,end=" ")
+        if t == 2:  # 두 자릿수인 경우
+            if i_str[0] == '3' or i_str[0] == '6' or i_str[0] == '9':
+                print(0, end=" ")
+            elif i_str[1] == '3' or i_str[1] == '6' or i_str[1] == '9':
+                print(0, end=" ")
             else:
                 print(i, end=" ")
-        else:
-            print(i,end=" ")
+        else:  # 한 자릿수인 경우
+            if i_str == '3' or i_str == '6' or i_str == '9':
+                print(0, end=" ")
+            else:
+                print(i, end=" ")
