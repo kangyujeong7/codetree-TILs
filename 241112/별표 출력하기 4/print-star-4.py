bbcce2db6c22e31090aeeef9c1264d78d48b0b9d
kangyujeong7 <jeong4696@gmail.com>
2024-11-12 15:58:14 +0900

    Title: 별표 출력하기 4, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241112/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,12 @@
+arr = input().strip().split()
+
+n = int(arr[0])
+
+for j in range(n):
+    for _ in range(n-j,0,-1):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for _ in range(i+2):
+        print("*",end=" ")
+    print()
